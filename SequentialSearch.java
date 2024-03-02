2bbba1b80d28f4577bc196b94d114002f11d9b1e
VGpallavi <161480731+VGpallavi@users.noreply.github.com>
2024-03-02 17:09:21 +0530

    Add files via upload

---
+++ SequentialSearch.java
@@ -0,0 +1,23 @@
+package week5;
+
+public class SequentialSearch {
+    public static int sequentialSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;  // Return the index if the target is found
+            }
+        }
+        return -1; // Return -1 if the target is not found
+    }
+
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50};
+        int target = 30;
+        int index = sequentialSearch(array, target);
+        if (index != -1) {
+            System.out.println("Element " + target + " found at index " + index + ".");
+        } else {
+            System.out.println("Element " + target + " not found in the array.");
+        }
+    }
+}
