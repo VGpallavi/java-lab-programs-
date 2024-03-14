056069fbd3a711f2afbb2796265307c2fa534436
VGpallavi <161480731+VGpallavi@users.noreply.github.com>
2024-03-14 19:42:03 +0530

    Add files via upload

---
+++ checkPositiveorNegative.java
@@ -0,0 +1,19 @@
+package week1;
+
+public class checkPositiveorNegative {
+	
+	public static void main(String args[]) {
+		int num=-902;
+		if(num>0)
+		{
+			System.out.println("the number is positive");
+			
+		}
+		else if(num<0)
+		{
+			System.out.println("the number is negative");
+		}
+	}
+
+}
+  
