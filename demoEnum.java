a3cab1eb19ad9821e832c70eb36b306c8d9987bf
VGpallavi <161480731+VGpallavi@users.noreply.github.com>
2024-04-29 18:03:47 +0530

    Add files via upload

---
+++ demoEnum.java
@@ -0,0 +1,34 @@
+package week12;
+enum Signal {
+	RED, YELLOW, GREEN
+}
+class checkSignal
+{
+	Signal currentSignal;
+	public checkSignal (Signal cs) {
+		this.currentSignal = cs;
+	}
+	public void makeRide ()
+	{
+		switch(currentSignal) 
+		{
+			case RED:
+				System.out.println("The signal is RED. \nKindly wait until it turns to GREEN.");
+				break;
+			case YELLOW:
+				System.out.println("The signal is YELLOW. \nKindly wait look around and make safe move.");
+				break;
+			case GREEN:
+				System.out.println("The signal is GREEN. \nHave safe Ride.");
+				break;
+		}
+	}
+}
+public class demoEnum
+{
+	public static void main(String[] args)
+	{
+		checkSignal myDrive = new checkSignal (Signal.GREEN); 
+		myDrive.makeRide();
+	}
+} 
